b4236d77d25132baa712d0ba4da408a2dc6a7a8e
egorko <dewvify@gmail.com>
2021-01-17 12:16:06 +0500

    add conditions range

--- react/src/filterProducts.jsx
+++ react/src/filterProducts.jsx
@@ -11,18 +11,56 @@ const FilterProducts = () => {
     const [isCheckPosition, setIsCheckPosition] = useState(0);
 
     const buttonMove = (moving ,pointX) => {
-   
-        if (isCheckPosition < 180)
-        {
-            isMoving && setFirstPointX((positionButton >= 25 )  ?  (10 + firstPointX) : -10 + firstPointX);
-            isMovingEndButton && setEndPointX(positionButton > 25 ? (10 + endPointX) : -10 + endPointX);
-            setIsCheckPosition(firstPointX-endPointX);
+      console.log(isCheckPosition);
+    //test need rework
+        if (isCheckPosition < 280)
+        {   
+            if (positionButton > 20)
+            {
+                isMoving && setFirstPointX((firstPointX + 5));
+                isMovingEndButton && setEndPointX((endPointX - 5));
+            }
+            if (positionButton < 10)
+            {
+                isMoving && setFirstPointX((firstPointX - 5));
+                isMovingEndButton && setEndPointX((endPointX + 5));
+            }
+            setIsCheckPosition(firstPointX+endPointX);
+
+            if (positionButton > 20)
+            {
+                if (endPointX <= 0)
+                {
+                    isMovingEndButton && setEndPointX((endPointX));
+                }
+                isMoving && setFirstPointX((firstPointX + 5));
+                }
+    
+            if (positionButton < 10)
+            {
+                if (firstPointX <= 0)
+                {
+                    isMoving && setFirstPointX((firstPointX));
+                }
+                isMovingEndButton && setEndPointX((endPointX + 5));
+            } 
         }
         else{
-            isMoving && setFirstPointX((positionButton >= 25 )  ?  (firstPointX) : -10 + firstPointX);
-            isMovingEndButton && setEndPointX(positionButton > 25 ? (10 + endPointX) : endPointX);
-            setIsCheckPosition(firstPointX-endPointX);
+            if (positionButton > 20)
+            {
+                isMoving && setFirstPointX((firstPointX));
+                isMovingEndButton && setEndPointX((endPointX - 5));
+            }
+
+            if (positionButton < 10)
+            {
+                isMoving && setFirstPointX((firstPointX - 5));
+                isMovingEndButton && setEndPointX((endPointX));
+            }
+            setIsCheckPosition(firstPointX+endPointX);
         }
+       
+       
     }
 
     return (
@@ -44,41 +82,43 @@ const FilterProducts = () => {
 
                 <div className="line-price">
                     <div className="into-line-price">   
-                 
-                        <div 
-                            onMouseUp={(e) => setIsMoving(false)}
-                            onMouseDown={(e) => setIsMoving(true)}  
-                            onMouseMove={(e) => setPositionButton(e.nativeEvent.offsetX)}
-                         
-                            style={
-                                {
-                                    marginLeft: `${firstPointX}px`
-                                }
-                            }
-                            className="first-point-line"
-                        >
-
-                        </div>
-
+                
                         <div 
                             onMouseUp={(e) => setIsMoving(false)}
                             onMouseMove={(e) => buttonMove()}
                             className="true-into-line-price">  
-                        </div>
 
-                        <div 
-                            onMouseUp={(e) => setIsMovingEndButton(false)}
-                            onMouseDown={(e) => setIsMovingEndButton(true)}  
-                            onMouseMove={(e) => setPositionButton(e.nativeEvent.offsetX)}
-                         
-                            style={
-                                {
-                                    marginLeft: `${endPointX}px`
+
+                            <div 
+                                onMouseUp={(e) => setIsMoving(false) && setIsMovingEndButton(false)}
+                                onMouseDown={(e) => setIsMoving(true)}  
+                                onMouseMove={(e) => setPositionButton(e.nativeEvent.offsetX)}
+                            
+                                style={
+                                    {
+                                        marginLeft: `${firstPointX}px`
+                                    }
                                 }
-                            }
+                                className="first-point-line"
+                            >
+                            </div>
 
-                            className="end-point-line">
+                            <div 
+                                onMouseUp={(e) => setIsMovingEndButton(false) && setIsMoving(false)}
+                                onMouseDown={(e) => setIsMovingEndButton(true)}  
+                                onMouseMove={(e) => setPositionButton(e.nativeEvent.offsetX)}
+                            
+                                style={
+                                    {
+                                        marginRight: `${endPointX}px`
+                                    }
+                                }
+
+                                className="end-point-line">
+                            </div>
+                       
                         </div>
+
                        
                     </div>
                 </div>
